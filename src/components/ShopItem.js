18727e9162e1c1f0a3cd1c5d6b5408600de2306f
thiago-porto25 <thiago_porto25@hotmail.com>
2021-08-20 12:32:08 -0300

    99% finish shop page for desktops

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,8 +1,72 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div``
+const Button = styled.button`
+  width: 9rem;
+  height: 2.5rem;
+  border: none;
+  border-radius: 5px;
+  background-color: black;
+  color: #e85a4f;
+  font-size: 19px;
+  font-weight: bold;
+  margin-top: 10px;
+  margin-bottom: 30px;
+`
 
-export default function ShopItem(props) {
-  return <Container>shop Item</Container>
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: white;
+  width: 250px;
+  margin: 20px;
+  border-radius: 5px;
+  border: 1px solid #bbbbbb;
+`
+
+const InfoContainer = styled.div`
+  width: 80%;
+  h3 {
+    text-align: center;
+    word-wrap: break-word;
+  }
+`
+
+const ImageContainer = styled.div`
+  width: 200px;
+  height: 220px;
+  overflow-y: hidden;
+  display: flex;
+  justify-content: center;
+  border-bottom: 1px solid #aaaaaa;
+
+  img {
+    margin-top: 10px;
+    margin-bottom: 10px;
+    width: auto;
+    height: 200px;
+    object-fit: cover;
+    overflow: hidden;
+  }
+`
+
+export default function ShopItem({ item }) {
+  const handleClick = () => {
+    console.log('test')
+  }
+
+  return (
+    <Container>
+      <ImageContainer>
+        <img src={`/images/products/${item.id}.jpg`} alt={item.name} />
+      </ImageContainer>
+      <InfoContainer>
+        <h3>{item.name}</h3>
+        <p>${(Math.round(item.price * 100) / 100).toFixed(2)}</p>
+      </InfoContainer>
+      <Button onClick={handleClick}>Buy</Button>
+    </Container>
+  )
 }
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,14 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Header, Footer } from '../components'
+import { Header, Footer, ShopItem } from '../components'
+import productsData from '../data/productsData'
 
-const Container = styled.div``
+const Container = styled.div`
+  background: linear-gradient(120deg, #999999, #eeeeee);
+  height: fit-content;
+`
+
+const Inner = styled.div`
+  margin: auto;
+  width: 70%;
+  min-width: 700px;
+  height: fit-content;
+  display: grid;
+  justify-items: center;
+  gap: 30px;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  padding: 30px 0px 30px 0px;
+`
 
 export default function Shop({ cart }) {
   return (
     <Container>
       <Header cart={cart} />
-      Shop
+      <Inner>
+        {productsData.map((item) => (
+          <ShopItem key={item.id} item={item} />
+        ))}
+      </Inner>
       <Footer />
     </Container>
   )
